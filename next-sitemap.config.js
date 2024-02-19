0a6689e4b1a3578d0a3da8398208325cc48cd37b
jmechristian <jmechristian@gmail.com>
2024-02-19 16:14:36 -0500

    sss

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.schoolgirlstyle.com',
+  changefreq: 'daily',
+  priority: 0.7,
   generateRobotsTxt: true, // (optional)
   exclude: ['/edit/*', '/server-sitemap-index.xml'],
   robotsTxtOptions: {
--- pages/server-sitemap-index.xml/index.js
+++ pages/server-sitemap-index.xml/index.js
@@ -1,10 +1,13 @@
 // pages/server-sitemap-index.xml/index.tsx
 import { getServerSideSitemapIndexLegacy } from 'next-sitemap';
+import { getAllPostsWithSlug } from '../../lib/API';
 
 export const getServerSideProps = async (ctx) => {
   // Method to source urls from cms
   const allPosts = await getAllPostsWithSlug();
-  const paths = allPosts.nodes.map((post) => post.slug);
+  const paths = allPosts.nodes.map(
+    (post) => 'https://www.schoolgirlstyle.com/' + post.slug
+  );
 
   return getServerSideSitemapIndexLegacy(ctx, paths);
 };
